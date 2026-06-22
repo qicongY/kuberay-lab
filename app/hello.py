de44e52163cc5d2dcb46c826beeab77850a2ee4c
qicongY <94815075+qicongY@users.noreply.github.com>
2026-06-22 21:25:19 +0800

    Modify Hello class for async call and autoscaling

--- app/hello.py
+++ app/hello.py
@@ -1,8 +1,16 @@
 from ray import serve
 import socket
 import os
+import time
 
-@serve.deployment(num_replicas=3)
+@serve.deployment(
+    autoscaling_config={
+        "min_replicas":1,
+        "max_replicas":10,
+        "target_ongoing_requests":2
+    }
+)
+
 class Hello:
     def __init__(self):
         # 缓存主机名，避免每次请求都调用系统函数
@@ -10,7 +18,8 @@ class Hello:
         # 如果需要具体的 Pod IP，可以通过环境变量获取
         self.pod_ip = os.environ.get("MY_POD_IP", "unknown")
 
-    def __call__(self):
-        return f"hello rayserve:v2, running on host: {self.hostname}, pod_ip: {self.pod_ip}"
+    async def __call__(self):
+            time.sleep(5)
+            return f"hello rayserve:v2, running on host: {self.hostname}, pod_ip: {self.pod_ip}"
 
 deployment_graph = Hello.bind()
